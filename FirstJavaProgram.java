1d942edfc13e047a01248c6772d067f4d5da514f
Nouf Alz- <106008801+noufmohammad@users.noreply.github.com>
2023-06-22 21:36:36 +0300

    Delete FirstJavaProgram.java

--- FirstJavaProgram.java
+++
@@ -1,7 +0,0 @@
-public class FirstJavaProgram{
-    public static void main (String[]args){
-    System.out.println("MY name is Coding Dojo");    
-    System.out.println("I am 100 Years old");    
-    System.out.println(" My hometown is Burbank,CA");    
-    }
-}
